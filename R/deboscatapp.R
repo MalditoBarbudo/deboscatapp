8ba384826e0f13d6fe946ad39a3740ac72f151bb
MalditoBarbudo <victorgrandagarcia@gmail.com>
2021-11-22 14:16:57 +0100

    remove spinners argument from waiter0

--- R/deboscatapp.R
+++ R/deboscatapp.R
@@ -56,7 +56,7 @@ $(document).on('shiny:disconnected', function(event) {
     shinyjs::useShinyjs(),
 
     # use waiter and waitress
-    waiter::use_waiter(spinners = 4),
+    waiter::use_waiter(),
     waiter::use_hostess(),
 
     # css
